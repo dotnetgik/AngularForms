8c3539235002330490287b6154acda5c849ec23d
MangeshG <Mangesh.g@intelgain.com>
2017-12-21 19:36:30 +0530

    Added changes for the validations  Forms

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {NgForm} from '@angular/forms'
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -9,7 +9,16 @@ export class SignupComponent implements OnInit {
 
   constructor() { }
 
+  valuesubmited:string="";
+
   ngOnInit() {
   }
 
+
+  registerUser(form:NgForm)
+  {
+    this.valuesubmited=form.value;
+  console.log(form);
+  //form.reset();
+  }
 }
